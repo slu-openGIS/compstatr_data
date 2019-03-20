1725ed648fe85c958656bddcb87d00bb37007ec7
Christopher Prener <chris.prener@slu.edu>
2019-03-20 10:37:49 -0500

    add rda creation line to script

--- source/year17.R
+++ source/year17.R
@@ -6,6 +6,7 @@ library(here)
 library(readr)
 library(usethis)
 
+# import each month and take first 20 rows
 jan17 <- read_csv(here("data", "raw", "2017", "january2017.csv")) %>%
   slice(1:20)
 
@@ -42,4 +43,5 @@ nov17 <- read_csv(here("data", "raw", "2017", "november2017.csv")) %>%
 dec17 <- read_csv(here("data", "raw", "2017", "december2017.csv")) %>%
   slice(1:20)
 
-
+#save to rda file
+save(jan17, feb17, mar17, apr17, may17, jun17, jul17, aug17, sep17, nov17, dec17, file = here("data", "year17.rda"))
