4ce2713496533ecade80b0abc048e62bf6d7213d
Christopher Prener <chris.prener@slu.edu>
2019-03-20 13:59:05 -0500

    create 2018 year-list object

---
+++ source/year18.R
@@ -0,0 +1,64 @@
+# create 2018 object
+
+# dependencies
+library(compstatr)
+library(dplyr)
+library(here)
+library(readr)
+library(usethis)
+
+# import each month and take first 20 rows
+read_csv(here("data", "raw", "2018", "january2018.csv")) %>%
+  slice(1:20) %>%
+  write_csv(here("data", "intermediate", "2018", "january2018.csv"))
+
+read_csv(here("data", "raw", "2018", "february2018.csv")) %>%
+  slice(1:20) %>%
+  write_csv(here("data", "intermediate", "2018", "february2018.csv"))
+
+read_csv(here("data", "raw", "2018", "march2018.csv")) %>%
+  slice(1:20) %>%
+  write_csv(here("data", "intermediate", "2018", "march2018.csv"))
+
+read_csv(here("data", "raw", "2018", "april2018.csv")) %>%
+  slice(1:20) %>%
+  write_csv(here("data", "intermediate", "2018", "april2018.csv"))
+
+read_csv(here("data", "raw", "2018", "may2018.csv")) %>%
+  slice(1:20) %>%
+  write_csv(here("data", "intermediate", "2018", "may2018.csv"))
+
+read_csv(here("data", "raw", "2018", "june2018.csv"))  %>%
+  slice(1:20) %>%
+  write_csv(here("data", "intermediate", "2018", "june2018.csv"))
+
+read_csv(here("data", "raw", "2018", "july2018.csv")) %>%
+  slice(1:20) %>%
+  write_csv(here("data", "intermediate", "2018", "july2018.csv"))
+
+read_csv(here("data", "raw", "2018", "august2018.csv")) %>%
+  slice(1:20) %>%
+  write_csv(here("data", "intermediate", "2018", "august2018.csv"))
+
+read_csv(here("data", "raw", "2018", "september2018.csv")) %>%
+  slice(1:20) %>%
+  write_csv(here("data", "intermediate", "2018", "september2018.csv"))
+
+read_csv(here("data", "raw", "2018", "october2018.csv")) %>%
+  slice(1:20) %>%
+  write_csv(here("data", "intermediate", "2018", "october2018.csv"))
+
+read_csv(here("data", "raw", "2018", "november2018.csv")) %>%
+  slice(1:20) %>%
+  write_csv(here("data", "intermediate", "2018", "november2018.csv"))
+
+read_csv(here("data", "raw", "2018", "december2018.csv")) %>%
+  slice(1:20) %>%
+  write_csv(here("data", "intermediate", "2018", "december2018.csv"))
+
+yearList18 <- cs_load_year(path = here("data", "intermediate", "2018"))
+
+cs_validate_year(yearList18, year = 2018)
+
+use_data(yearList18)
+
